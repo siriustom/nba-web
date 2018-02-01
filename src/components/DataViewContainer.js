0e12f41adbb84a896e5fdedae651e7211093460e
siriustom <linxiaoran0210@gmail.com>
2018-01-31 20:47:51 -0800

    add radio group filter

--- src/components/DataViewContainer.js
+++ src/components/DataViewContainer.js
@@ -1,22 +1,45 @@
 import React from 'react';
 import { ShotChart } from './ShotChart';
 import { CountSlider} from './CountSlider';
+import { Radio } from 'antd';
+import { Row, Col } from 'antd';
+const RadioGroup = Radio.Group;
 
 export class DataViewContainer extends React.Component {
     state = {
         minCount: 2,
+        chartType: 'hexbin',
     }
 
     onCountSliderChange = (count) => {
         this.setState({minCount: count});
     }
 
+    onChartTypeChange = (e) => {
+        this.setState({chartType: e.target.value});
+    }
+
     render() {
         return (
             <div className="data-view">
-                <ShotChart playerId={this.props.playerId} minCount={this.state.minCount}/>
+                <ShotChart
+                    playerId={this.props.playerId}
+                    minCount={this.state.minCount}
+                    chartType={this.state.chartType}
+                />
                 <div className="filters">
-                    <CountSlider onCountSliderChange={this.onCountSliderChange}/>
+                    {this.state.chartType === "hexin" && <CountSlider onCountSliderChange={this.onCountSliderChange}/>}
+                    <Row>
+                        <Col span={12} offset={3}>
+                            <RadioGroup onChange={this.onChartTypeChange} value={this.state.chartType}>
+                                <Radio value={'hexbin'}>Hexbin</Radio>
+                                <Radio value={'scatter'}>Scatter</Radio>
+                            </RadioGroup>
+                        </Col>
+                        <Col span={6}>
+
+                        </Col>
+                    </Row>
                 </div>
             </div>
         );
