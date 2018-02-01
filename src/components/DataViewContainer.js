ebf86030e38bf626abb7c67cfeacb9d10b5a9d8d
siriustom <linxiaoran0210@gmail.com>
2018-01-31 21:01:26 -0800

    add toggle tooltip

--- src/components/DataViewContainer.js
+++ src/components/DataViewContainer.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { ShotChart } from './ShotChart';
 import { CountSlider} from './CountSlider';
-import { Radio } from 'antd';
-import { Row, Col } from 'antd';
+import { Radio, Row, Col, Switch } from 'antd';
 const RadioGroup = Radio.Group;
 
 export class DataViewContainer extends React.Component {
     state = {
         minCount: 2,
         chartType: 'hexbin',
+        displayTooltip: true,
     }
 
     onCountSliderChange = (count) => {
@@ -19,6 +19,9 @@ export class DataViewContainer extends React.Component {
         this.setState({chartType: e.target.value});
     }
 
+    onTooltipChange = (displayTooltip) => {
+        this.setState({displayTooltip});
+    }
     render() {
         return (
             <div className="data-view">
@@ -26,18 +29,23 @@ export class DataViewContainer extends React.Component {
                     playerId={this.props.playerId}
                     minCount={this.state.minCount}
                     chartType={this.state.chartType}
+                    displayTooltip={this.state.displayTooltip}
                 />
                 <div className="filters">
                     {this.state.chartType === "hexin" && <CountSlider onCountSliderChange={this.onCountSliderChange}/>}
                     <Row>
-                        <Col span={12} offset={3}>
+                        <Col span={12} offset={2}>
                             <RadioGroup onChange={this.onChartTypeChange} value={this.state.chartType}>
                                 <Radio value={'hexbin'}>Hexbin</Radio>
                                 <Radio value={'scatter'}>Scatter</Radio>
                             </RadioGroup>
                         </Col>
                         <Col span={6}>
-
+                            <Switch checkedChildren="On"
+                                    unCheckedChildren="Off"
+                                    defaultChecked
+                                    onChange={this.onTooltipChange}
+                            />
                         </Col>
                     </Row>
                 </div>
